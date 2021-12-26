0eee240f81fb30ee49694a0ef56341ec46f05ac6
dtarroz <damien.tarroz.apps@gmail.com>
2021-12-26 09:21:00 +0100

    feat: initial dev

--- src/ItsMyConsole.Tools.GlobalCache/Services/GlobalCacheTools.cs
+++ src/ItsMyConsole.Tools.GlobalCache/Services/GlobalCacheTools.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+
 namespace ItsMyConsole.Tools.GlobalCache
 {
     /// <summary>
@@ -5,6 +8,8 @@ namespace ItsMyConsole.Tools.GlobalCache
     /// </summary>
     public class GlobalCacheTools
     {
+        private readonly Dictionary<string, object> _globalCache = new Dictionary<string, object>();
+
         internal GlobalCacheTools() { }
 
         /// <summary>
@@ -12,14 +17,25 @@ namespace ItsMyConsole.Tools.GlobalCache
         /// </summary>
         /// <param name="key">La clé unique qui représente la donnée dans le cache</param>
         /// <param name="value">La valeur à ajouter ou mettre à jour dans le cache associée à la clé</param>
-        public void Set(string key, object value) { }
+        public void Set(string key, object value) {
+            ThrowIfKeyIsNotValid(key, nameof(key));
+            _globalCache[key] = value;
+        }
+
+        private static void ThrowIfKeyIsNotValid(string key, string name) {
+            switch (key) {
+                case null: throw new ArgumentNullException(name);
+                case "":   throw new ArgumentException("value not empty", name);
+            }
+        }
 
         /// <summary>
         /// Récupérer la valeur dans le cache associée à la clé
         /// </summary>
         /// <param name="key">La clé unique qui représente la donnée dans le cache</param>
         public T Get<T>(string key) {
-            return default;
+            ThrowIfKeyIsNotValid(key, nameof(key));
+            return _globalCache.ContainsKey(key) ? (T)_globalCache[key] : default;
         }
 
         /// <summary>
@@ -29,14 +45,19 @@ namespace ItsMyConsole.Tools.GlobalCache
         /// <param name="value">La valeur en cache si la clé associée existe</param>
         /// <returns>true si la clé existe</returns>
         public bool TryGetValue<T>(string key, out T value) {
-            value = default;
-            return false;
+            ThrowIfKeyIsNotValid(key, nameof(key));
+            bool isExists = _globalCache.ContainsKey(key);
+            value = isExists ? (T)_globalCache[key] : default;
+            return isExists;
         }
 
         /// <summary>
         /// Supprimer la valeur dans le cache associée à la clé
         /// </summary>
         /// <param name="key">La clé unique qui représente la donnée dans le cache</param>
-        public void Remove(string key) { }
+        public void Remove(string key) {
+            ThrowIfKeyIsNotValid(key, nameof(key));
+            _globalCache.Remove(key);
+        }
     }
 }
